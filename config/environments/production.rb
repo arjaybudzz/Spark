b10bc8711e586a3f084a8f5bd927266eabc3ab51
arjaybudzz <archessonan.1@gmail.com>
2023-10-09 20:10:59 +0800

    changed backend url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  host = 'https://spark-9bqv.onrender.com' #replace with your own url
+  host = 'http://0.0.0.0:3001' #replace with your own url
   config.action_mailer.default_url_options = { host: host }
 
   # SMTP settings for gmail
