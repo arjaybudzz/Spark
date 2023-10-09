dcb8b3e46ef147b72d4a494f37186b2b50ba684a
arjaybudzz <archessonan.1@gmail.com>
2023-10-09 21:28:41 +0800

    replaced frontend url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
+    origins "https://spark-frontend.onrender.com"
 
     resource "*",
     headers: :any,
